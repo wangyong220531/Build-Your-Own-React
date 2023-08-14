2da784e26a14ae23808e59208215b0f86af53817
王勇 <wy19945372694@163.com>
2023-08-14 15:22:55 +0800

    add render2.js

--- render2.js
+++ render2.js
@@ -3,11 +3,25 @@ function render(element, parentDom) {
     const dom = createElement(type)
 
     const isListener = name => name.startsWith("on")
+
     Object.keys(props)
         .filter(isListener)
         .forEach(name => {
             const eventType = name.toLocaleLowerCase().substring(2)
             dom.addEventListener(eventType, props[name])
         })
+
     const isAttribute = name => !isListener(name) && name !== "children"
+
+    Object.keys(props)
+        .filter(isAttribute)
+        .forEach(name => {
+            dom[name] = props[name]
+        })
+
+    const childElements = props.children || []
+
+    childElements.forEach(childElement => render(childElement, dom))
+
+    parentDom.appendChild(dom)
 }
