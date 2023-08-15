2f3fd4f88ea65d2009b32f6876f1501947fa76a6
王勇 <wy19945372694@163.com>
2023-08-15 11:25:21 +0800

    update index.js and weact.js

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 // import Weact from "./weact"
 
-const Weact = importFromBelow()
+const stories = [
+    { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
+    { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
+    { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
+    { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
+    { name: "Components and state", url: "http://bit.ly/2rE16nh" }
+]
 
-const app = {
+const appElement = {
     type: "div",
     props: {
         children: [
             {
-                type: "div",
+                type: "ul",
                 props: {
-                    children: [{ type: "TEXT ELEMENT", nodeValue: "123" }]
+                    children: stories.map(storyElement)
                 }
             }
         ]
     }
 }
+
+function storyElement({ name, url }) {
+    const likes = Math.ceil(Math.random() * 100)
+    const buttonElement = {
+        type: "button",
+        props: {
+            children: [
+                { type: "TEXT ELEMENT", props: { nodeValue: likes } },
+                { type: "TEXT ELEMENT", props: { nodeValue: "❤️" } }
+            ]
+        }
+    }
+    const linkElement = {
+        type: "a",
+        props: {
+            href: url,
+            children: [{ type: "TEXT ELEMENT", props: { nodeValue: name } }]
+        }
+    }
+    return {
+        type: "li",
+        props: {
+            children: [buttonElement, linkElement]
+        }
+    }
+}
+
+const Weact = importFromBelow()
 
 function importFromBelow() {
     function render(element, parentDom) {
@@ -54,4 +88,4 @@ function importFromBelow() {
     }
 }
 
-Weact.render(app, document.getElementById("root"))
+Weact.render(appElement, document.getElementById("root"))
