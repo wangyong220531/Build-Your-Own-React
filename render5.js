4ec645ac63f90e6d5ee4c03a7037b68f7ae55d0f
王勇 <wy19945372694@163.com>
2023-08-15 10:40:20 +0800

    add reconcile.js and component.js

--- render5.js
+++ render5.js
@@ -6,44 +6,49 @@ function render(element, container) {
     rootInstance = nextInstance
 }
 
-function reconcile(parentDOM, instance, element) {
-    if (instance == null) {
-        const newInstance = instantiate(element)
-        parentDOM.appendChild(newInstance.dom)
-        return newInstance
-    } else {
-        const newInstance = instantiate(element)
-        parentDOM.replaceChild(newInstance.dom, instance.dom)
-        return newInstance
-    }
-}
-
 function instantiate(element) {
     const { type, props } = element
+    const isDomElement = typeof type === "string"
 
-    // Create DOM Element
-    const isTextElement = type === "TEXT_ELEMENT"
-    const dom = isTextElement ? document.createElement("") : document.createElement(type)
+    if (isDomElement) {
+        // Instantiate DOM Element
+        const isTextElement = type === "TEXT_ELEMENT"
+        const dom = isTextElement ? document.createElement("") : document.createElement(type)
 
-    // Add event listner
-    const isListener = name => name.startWith("on")
-    Object.keys(props)
-        .filter(isListener)
-        .forEach(name => {
-            const eventType = name.toLowerCase().substring(2)
-            dom.addEventListener(eventType, props[name])
-        })
+        // Add event listner
+        const isListener = name => name.startWith("on")
+        Object.keys(props)
+            .filter(isListener)
+            .forEach(name => {
+                const eventType = name.toLowerCase().substring(2)
+                dom.addEventListener(eventType, props[name])
+            })
 
-    // Set properties
-    const isAttribute = name => !isListener(name) && name !== "children"
-    Object.keys(props)
-        .filter(isAttribute)
-        .forEach(name => {
-            dom[name] = props[name]
-        })
+        // Set properties
+        const isAttribute = name => !isListener(name) && name !== "children"
+        Object.keys(props)
+            .filter(isAttribute)
+            .forEach(name => {
+                dom[name] = props[name]
+            })
 
-    // Instantiate and append children
-    const childElements = props.children || []
-    const childInstances = childElements.map(instantiate)
-    const childDoms = childInstances.map(childInstance => childInstance.dom)
+        // Instantiate and append children
+        const childElements = props.children || []
+        const childInstances = childElements.map(instantiate)
+        const childDoms = childInstances.map(childInstance => childInstance.dom)
+        childDoms.forEach(childDom => dom.appendChild(childDom))
+
+        const instance = { dom, element, childInstances }
+        return instance
+    } else {
+        // Instantiate component element
+        const instance = {}
+        const publicInstance = createPublicInstance(element, instance)
+        const childElement = publicInstance.render()
+        const childInstance = instantiate(childElement)
+        const dom = childInstance.dom
+
+        Object.assign(instance, { dom, element, childInstance, publicInstance })
+        return instance
+    }
 }
