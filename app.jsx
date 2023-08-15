d9de9042f395bdce4fc03ecc2ae39e52b0ada3dc
王勇 <wy19945372694@163.com>
2023-08-15 11:22:55 +0800

    add index.js and weact.js

---
+++ app.jsx
@@ -0,0 +1,9 @@
+const stories = [
+    { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
+    { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
+    { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
+    { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
+    { name: "Components and state", url: "http://bit.ly/2rE16nh" }
+]
+
+// class App extends 
